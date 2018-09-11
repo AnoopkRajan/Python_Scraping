ad6f73f4f9695b35e48b2307a259a8c1a289adbf
AnoopkRajan <41849778+AnoopkRajan@users.noreply.github.com>
2018-09-11 16:47:44 +0530

    Update python_scraping.py

--- python_scraping.py
+++ python_scraping.py
@@ -1,20 +1,9 @@
 from requests import get 
-
-
 from bs4 import BeautifulSoup
 
-
 url = "https://google.com/search?q=foss@amrita"
-
 response = get(url=url)
-
-
 extract = BeautifulSoup(response.text,'lxml')
-
-
 titles = extract.find_all('h3')
-
-
-
 for title in titles:
 	print(title.get_text())
